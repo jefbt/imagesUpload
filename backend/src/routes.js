632b18504812314e5407ed80ad0eaa5c8acd0c4d
Jeferson Tomazella <jefbt@outlook.com>
2020-03-09 16:55:47 -0300

    docs/backend: refactor and comment

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,34 +2,22 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
 
-const Image = require("./models/Image");
+const imageController = require("./controllers/imageController");
 
-routes.get("/images", async (req, res) => {
-  const images = await Image.find();
-  return res.json(images);
-});
-
-routes.post("/images/upload", multer(multerConfig).single("file"),
-  async (req, res) => {
-    const { originalName: name, size, key, location: url = "" } = req.file;
-
-    const post = await Image.create({
-      name,
-      size,
-      key,
-      url
-    });
-
-    return res.json({
-      image: post
-    });
-  }
+routes.get(
+  "/images",
+  imageController.listImages
 );
 
-routes.delete("/images/:id", async (req, res) => {
-  const image = await Image.findById(req.params.id);
-  await image.remove();
-  return res.send("Image removed");
-});
+routes.post(
+  "/images/upload",
+  multer(multerConfig).single("file"),
+  imageController.uploadImage
+);
+
+routes.delete(
+  "/images/:id", 
+  imageController.deleteImage
+);
 
 module.exports = routes;
